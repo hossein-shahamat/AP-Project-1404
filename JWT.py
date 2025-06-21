a7b7fef76f54cfc02a8f86387e7bb79dc87f1feb
hossein <98hossein2006@gmail.com>
2025-06-21 13:23:29 +0330

    تنظیمات رمزنگاری

--- JWT.py
+++ JWT.py
@@ -7,3 +7,10 @@ from datetime import datetime , timedelta
 from sqlalchemy import create_engine , column , integer , String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker , session
+
+#ایجاد یک محیط رمز نگاری
+pwd_context = CryptContext(schemes=["pass"] , deprecated="auto")
+secret_key = "my_secret_key"
+algorithm = "HS256"
+expite_token = 30
+
