738cda697165e5d3e6f528763369f3c2bc0918dd
hossein <98hossein2006@gmail.com>
2025-06-21 12:12:20 +0330

    add library

--- JWT.py
+++ JWT.py
@@ -1,1 +1,9 @@
-print("hello word!")
+from fastapi import FastAPI , Depends , HTTPException , status
+from pydantic import BaseModel
+from typing import Optional
+from passlib.context import CryptContext
+import JWT
+from datetime import datetime , timedelta
+from sqlalchemy import create_engine , column , integer , String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker , session
